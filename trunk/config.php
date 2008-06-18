0337890254789d268ee90adfdc9ce894cb5d540c
Nat Welch <nat@natwelch.com>
2008-06-18 00:31:05 +0000

    Wow.... stupid stupid nat
    git-svn-id: http://svn.devjavu.com/ideaius@12 c546ad1a-3823-4856-a67e-ad82c513496f

--- trunk/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$db_user = "nat";
-$db_pass = "nat100";
-
-?>
--- trunk/config_EDIT.php
+++ trunk/config_EDIT.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Edit this and rename it to config.php to get everything working.
+ */
+
 
 $db_user = "";
 $db_pass = "";

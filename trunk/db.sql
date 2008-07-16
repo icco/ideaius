d2b21b7e9dff9c7c5f6df25c3bec1369c34ee8bc
Nat Welch <nat@natwelch.com>
2008-07-16 21:36:07 +0000

    Added the new database stuff, security, etc. to the schema. Still need to implement and test.
    git-svn-id: http://svn.devjavu.com/ideaius@21 c546ad1a-3823-4856-a67e-ad82c513496f

--- trunk/db.sql
+++ trunk/db.sql
@@ -4,9 +4,9 @@
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
-  `name` text NOT NULL,
-  `ID` int(11) NOT NULL auto_increment,
-  PRIMARY KEY  (`ID`)
+	`name` text NOT NULL,
+	`ID` int(11) NOT NULL auto_increment,
+	PRIMARY KEY  (`ID`)
 ) ENGINE=MyISAM;
 
 --
@@ -15,13 +15,13 @@ CREATE TABLE `categories` (
 
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
-  `ID` bigint(20) NOT NULL auto_increment,
-  `content` text NOT NULL,
-  `time` datetime NOT NULL,
-  `user` bigint(20) NOT NULL,
-  `wikiptr` bigint(20) default NULL,
-  `catID` bigint(20) NOT NULL,
-  PRIMARY KEY  (`ID`)
+	`ID` bigint(20) NOT NULL auto_increment,
+	`content` text NOT NULL,
+	`time` datetime NOT NULL,
+	`user` bigint(20) NOT NULL,
+	`wikiptr` bigint(20) default NULL,
+	`cID` bigint(20) NOT NULL,
+	PRIMARY KEY  (`ID`)
 ) ENGINE=MyISAM;
 
 --
@@ -30,10 +30,9 @@ CREATE TABLE `posts` (
 
 DROP TABLE IF EXISTS `t2p`;
 CREATE TABLE `t2p` (
-  `ID` bigint(20) NOT NULL auto_increment,
-  `pID` int(11) NOT NULL,
-  `tID` int(11) NOT NULL,
-  PRIMARY KEY  (`ID`)
+	`pID` int(11) NOT NULL,
+	`tID` int(11) NOT NULL,
+	PRIMARY KEY  (`tID`)
 ) ENGINE=MyISAM;
 
 --
@@ -42,9 +41,9 @@ CREATE TABLE `t2p` (
 
 DROP TABLE IF EXISTS `tags`;
 CREATE TABLE `tags` (
-  `ID` int(11) NOT NULL auto_increment,
-  `name` text NOT NULL,
-  PRIMARY KEY  (`ID`)
+	`ID` int(11) NOT NULL auto_increment,
+	`name` text NOT NULL,
+	PRIMARY KEY  (`ID`)
 ) ENGINE=MyISAM;
 
 --
@@ -53,15 +52,15 @@ CREATE TABLE `tags` (
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `ID` bigint(20) unsigned NOT NULL auto_increment,
-  `RealName` text,
-  `uname` text NOT NULL,
-  `email` text NOT NULL,
-  `passwd` text NOT NULL,
-  `bday` date default NULL,
-  `location` text,
-  `created` datetime NOT NULL,
-  PRIMARY KEY  (`ID`)
+	`ID` bigint(20) unsigned NOT NULL auto_increment,
+	`RealName` text,
+	`uname` text NOT NULL,
+	`email` text NOT NULL,
+	`passwd` text NOT NULL,
+	`bday` date default NULL,
+	`location` text,
+	`created` datetime NOT NULL,
+	PRIMARY KEY  (`ID`)
 ) ENGINE=MyISAM;
 
 --
@@ -70,11 +69,35 @@ CREATE TABLE `users` (
 
 DROP TABLE IF EXISTS `wiki`;
 CREATE TABLE `wiki` (
-  `ID` bigint(20) NOT NULL auto_increment,
-  `pID` bigint(20) NOT NULL,
-  `content` mediumtext NOT NULL,
-  `date` datetime NOT NULL,
-  `uID` bigint(20) NOT NULL,
-  PRIMARY KEY  (`ID`)
+	`ID` bigint(20) NOT NULL auto_increment,
+	`pID` bigint(20) NOT NULL,
+	`content` mediumtext NOT NULL,
+	`date` datetime NOT NULL,
+	`uID` bigint(20) NOT NULL,
+	PRIMARY KEY  (`ID`)
+) ENGINE=MyISAM;
+
+--
+-- Table structure for table `groups`
+--
+DROP TABLE IF EXISTS `groups`;
+CREATE TABLE `groups` (
+	`ID` bigint(20) NOT NULL auto_increment,
+	`pID` bigint(20) NOT NULL,
+	`name` text NOT NULL,
+	`usercount` int NOT NULL,
+	PRIMARY KEY  (`ID`)
+) ENGINE=MyISAM;
+
+--
+-- Table structure for table `security`
+--
+DROP TABLE IF EXISTS `security`;
+CREATE TABLE `security` (
+	`uID` bigint(20) NOT NULL,
+	`gID` bigint(20) NOT NULL,
+	`pID` bigint(20) NOT NULL,
+	`permissions` int NOT NULL,
+	PRIMARY KEY (`PID`)
 ) ENGINE=MyISAM;
 

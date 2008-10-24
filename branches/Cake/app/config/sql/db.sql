a56363ef54d1cbf4ccff5d95b4d5b1b2b5171eca
Nat Welch <nat@natwelch.com>
2008-10-24 20:01:57 +0000

    This new database schema could be all wrong... /Nat
    git-svn-id: http://svn.devjavu.com/ideaius@63 c546ad1a-3823-4856-a67e-ad82c513496f

--- branches/Cake/app/config/sql/db.sql
+++ branches/Cake/app/config/sql/db.sql
@@ -4,10 +4,10 @@
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
-	`name` text NOT NULL,
-	`ID` int(11) NOT NULL auto_increment,
-	PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM;
+	`cname` text NOT NULL,
+	`cID` int(11) NOT NULL auto_increment,
+	PRIMARY KEY  (`cID`)
+) ENGINE=InnoDB;
 
 --
 -- Table structure for table `posts`
@@ -15,14 +15,16 @@ CREATE TABLE `categories` (
 
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
-	`ID` bigint(20) NOT NULL auto_increment,
+	`pID` bigint(20) NOT NULL auto_increment,
 	`content` text NOT NULL,
-	`time` datetime NOT NULL,
-	`user` bigint(20) NOT NULL,
+	`time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`uID` bigint(20) NOT NULL,
 	`wikiptr` bigint(20) default NULL,
 	`cID` bigint(20) NOT NULL,
-	PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM;
+	PRIMARY KEY  (`pID`),
+	Foreign Key(`cID`) references categories,
+	Foreign Key(`uID`) references users
+) ENGINE=InnoDB;
 
 --
 -- Table structure for table `t2p`
@@ -33,7 +35,9 @@ CREATE TABLE `t2p` (
 	`pID` int(11) NOT NULL,
 	`tID` int(11) NOT NULL,
 	PRIMARY KEY  (`tID`)
-) ENGINE=MyISAM;
+	Foreign Key(`pID`) references posts 
+	Foreign Key(`tID`) references tags 
+) ENGINE=InnoDB;
 
 --
 -- Table structure for table `tags`
@@ -41,10 +45,10 @@ CREATE TABLE `t2p` (
 
 DROP TABLE IF EXISTS `tags`;
 CREATE TABLE `tags` (
-	`ID` int(11) NOT NULL auto_increment,
+	`tID` int(11) NOT NULL auto_increment,
 	`name` text NOT NULL,
-	PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM;
+	PRIMARY KEY  (`tID`)
+) ENGINE=InnoDB;
 
 --
 -- Table structure for table `users`
@@ -52,16 +56,16 @@ CREATE TABLE `tags` (
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-	`ID` bigint(20) unsigned NOT NULL auto_increment,
+	`uID` bigint(20) unsigned NOT NULL auto_increment,
 	`RealName` text,
 	`uname` text NOT NULL,
 	`email` text NOT NULL,
 	`passwd` text NOT NULL,
 	`bday` date default NULL,
 	`location` text,
-	`created` datetime NOT NULL,
-	PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM;
+	`created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY  (`uID`)
+) ENGINE=InnoDB;
 
 --
 -- Table structure for table `wiki`
@@ -70,25 +74,26 @@ CREATE TABLE `users` (
 DROP TABLE IF EXISTS `wiki`;
 DROP TABLE IF EXISTS `wikis`;
 CREATE TABLE `wikis` (
-	`ID` bigint(20) NOT NULL auto_increment,
+	`wID` bigint(20) NOT NULL auto_increment,
 	`pID` bigint(20) NOT NULL,
 	`content` mediumtext NOT NULL,
-	`date` datetime NOT NULL,
+	`date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	`uID` bigint(20) NOT NULL,
-	PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM;
+	PRIMARY KEY  (`wID`),
+	Foreign Key(`pID`) references posts 
+) ENGINE=InnoDB;
 
 --
 -- Table structure for table `groups`
 --
 DROP TABLE IF EXISTS `groups`;
 CREATE TABLE `groups` (
-	`ID` bigint(20) NOT NULL auto_increment,
+	`gID` bigint(20) NOT NULL auto_increment,
 	`pID` bigint(20) NOT NULL,
 	`name` text NOT NULL,
 	`usercount` int NOT NULL,
-	PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM;
+	PRIMARY KEY  (`gID`)
+) ENGINE=InnoDB;
 
 --
 -- Table structure for table `security`
@@ -99,6 +104,22 @@ CREATE TABLE `security` (
 	`gID` bigint(20) NOT NULL,
 	`pID` bigint(20) NOT NULL,
 	`permissions` int NOT NULL,
-	PRIMARY KEY (`PID`)
-) ENGINE=MyISAM;
+	PRIMARY KEY (`PID`),
+	Foreign Key(`pID`) references posts,
+	Foreign Key(`uID`) references users, 
+	Foreign Key(`gID`) references groups, 
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `u2g`
+--
+
+DROP TABLE IF EXISTS `u2g`;
+CREATE TABLE `u2g` (
+	`uID` int(11) NOT NULL,
+	`gID` int(11) NOT NULL,
+	PRIMARY KEY  (`gID`),
+	Foreign Key(`uID`) references users,
+	Foreign Key(`gID`) references groups, 
+) ENGINE=InnoDB;
 

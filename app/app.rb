4b2da6c4a5862613e7a22299fb9a8eea53274cac
Nat Welch <nat@natwelch.com>
2012-03-06 08:45:19 -0800

    cleaning up

--- app/app.rb
+++ app/app.rb
@@ -11,29 +11,8 @@ class Ideaus < Padrino::Application
     provider :github,   ENV['GITHUB_KEY'],   ENV['GITHUB_SECRET']
   end
 
+  set :show_exceptions, true
+
   use Rack::Lint
 
-  ##
-  # Caching support
-  #
-  # register Padrino::Cache
-  # enable :caching
-  #
-  # You can customize caching store engines:
-  #
-  #   set :cache, Padrino::Cache::Store::Memcache.new(::Memcached.new('127.0.0.1:11211', :exception_retry_limit => 1))
-  #   set :cache, Padrino::Cache::Store::Memcache.new(::Dalli::Client.new('127.0.0.1:11211', :exception_retry_limit => 1))
-  #   set :cache, Padrino::Cache::Store::Redis.new(::Redis.new(:host => '127.0.0.1', :port => 6379, :db => 0))
-  #   set :cache, Padrino::Cache::Store::Memory.new(50)
-  #   set :cache, Padrino::Cache::Store::File.new(Padrino.root('tmp', app_name.to_s, 'cache')) # default choice
-  #
-
-  ##
-  # Application configuration options
-  #
-  # set :raise_errors, true       # Raise exceptions (will stop application) (default for test)
-  # set :show_exceptions, true    # Shows a stack trace in browser (default for development)
-  # set :logging, true            # Logging in STDOUT for development and file for production (default only for development)
 end
-
-puts "in app.rb"
--- config/boot.rb
+++ config/boot.rb
@@ -1,17 +1,11 @@
-puts "In boot file."
-
 # Defines our constants
 PADRINO_ENV  = ENV["PADRINO_ENV"] ||= ENV["RACK_ENV"] ||= "development"  unless defined?(PADRINO_ENV)
 PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
-
-puts "Past constants."
 
 # Load our dependencies
 require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
 Bundler.require(:default, PADRINO_ENV)
-
-puts "Past bundler."
 
 ##
 # Enable devel logging

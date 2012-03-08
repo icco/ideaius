f4f19c7a624a23de7a115ffd27d03fad2e65f888
Nat Welch <nat@natwelch.com>
2012-03-07 21:03:17 -0800

    user editing

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -2,12 +2,22 @@ Ideaus.controllers :user do
   layout :main
 
   get '/new' do
+    if !session[:user]
+      render 'user/new'
+    else
+      redirect '/user/edit'
+    end
   end
 
   post '/new' do
   end
 
   get '/edit' do
+    if !session[:user]
+      redirect '/user/new'
+    end
+
+    render 'user/edit', :locals => { :user => User.find_by_id(session[:user]) }
   end
 
   post '/edit' do

adbedf4560b2ff6d41a13cb039fc18ff0a55f123
Nat Welch <nat@natwelch.com>
2012-03-09 18:29:44 -0800

    Will probably break things.

--- app/controllers/idea.rb
+++ app/controllers/idea.rb
@@ -1,20 +1,13 @@
 Ideaus.controllers :idea do
   layout :main
   
-  post :index do
+  post :new do
     i = Idea.new
     i.text = params["idea"]
     i.users_id = session[:user]
     i.save
 
     redirect "/idea/"
-  end
-
-  get :index do
-    user = logged_in!
-
-    ideas = Idea.all
-    render "idea/index", :locals => { :ideas => ideas, :user => user }
   end
 
   get :new do
--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -16,9 +16,14 @@ Ideaus.controller do
     redirect '/'
   end
 
-  #get '/:username' do
-  #  # get user profile page.
-  #end
+  # TODO: figure out how to do non-catchall routes...
+  get '/:username' do
+    # get user profile page.
+    user = logged_in!
+
+    ideas = Idea.where(:users_id => user.id)
+    render "idea/index", :locals => { :ideas => ideas, :user => user }
+  end
 
   #get '/:username/:project' do
   #  # get project page

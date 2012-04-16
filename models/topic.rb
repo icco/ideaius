31abb6a3861eaac917a8b92ca59644b4f37ba544
Nat Welch <nat@natwelch.com>
2012-04-16 00:01:41 -0700

    untested redis code.

--- models/topic.rb
+++ models/topic.rb
@@ -1,8 +1,26 @@
 class Topic < ActiveRecord::Base
   belongs_to :user
 
+  @@key = "#{self.user}:#{self}"
+
+  # In theory, we shouldn't have to block on this function, even though we do.
   def add_message msg
-    p msg
+    if msg.nil?
+      return false
+    end
+
+    REDIS.sadd @@key, msg.id
+    return REDIS.scard @@key
+  end
+
+  # Gets most resent messages
+  def messages count = 50
+    ids = []
+    REDIS.multi do
+      max = REDIS.scard @@key
+      min = Math.max(0, max-count)
+      ids = REDIS.lrange(@@key, min, max)
+    end
   end
 
   def to_s

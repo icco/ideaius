26adc7b4967ef86f30a50f2913be3c1812842f4d
Nat Welch <nat@natwelch.com>
2012-04-17 00:37:44 -0700

    message listing not working.

--- models/topic.rb
+++ models/topic.rb
@@ -1,26 +1,37 @@
 class Topic < ActiveRecord::Base
   belongs_to :user
 
-  @@key = "#{self.user}:#{self}"
-
   # In theory, we shouldn't have to block on this function, even though we do.
   def add_message msg
+    @key = "#{self.user}:#{self}"
+
     if msg.nil?
       return false
     end
 
-    REDIS.sadd @@key, msg.id
-    return REDIS.scard @@key
+    REDIS.sadd @key, msg.id
+    p REDIS.smembers @key
+    return REDIS.scard @key
   end
 
   # Gets most resent messages
   def messages count = 50
-    ids = []
+    @key = "#{self.user}:#{self}"
+
     REDIS.multi do
-      max = REDIS.scard @@key
-      min = Math.max(0, max-count)
-      ids = REDIS.lrange(@@key, min, max)
+      max = REDIS.scard @key
+      max = 0 if max.nil?
+      min = [0, max-count].max
+
+      if min != max
+        ids = REDIS.lrange(@key, min, max)
+      end
     end
+    ids = [] if ids.nil?
+
+    p ids
+
+    return Message.where(:id => ids)
   end
 
   def to_s

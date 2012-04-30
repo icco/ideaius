8ce5b63f4447dcdd114dc3e882a7143398fed050
Nat Welch <nat@natwelch.com>
2012-04-29 23:03:04 -0700

    an attempt to fix auth when user fakes cookie.

--- app/helpers/util_helpers.rb
+++ app/helpers/util_helpers.rb
@@ -1,10 +1,15 @@
 Stackius.helpers do
   def logged_in!
-    if !session[:user]
-      redirect '/user/new'
-    else
-      return User.find_by_id(session[:user])
+    if session[:user]
+      user = User.find_by_id(session[:user])
+      if !user.nil?
+        return user
+      else
+        redirect '/logout'
+      end
     end
+
+    redirect '/user/new'
   end
 end
 

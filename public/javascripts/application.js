5d13a25abbeb86981564182e6508698c363c5f16
Nat Welch <nat@natwelch.com>
2012-05-06 13:53:34 -0700

    stripping out some old css and js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,3 @@
 $(document).ready(function() {
-  var chatForm = $("#posttochat");
-  chatForm.submit(function(ev) {
-    event.preventDefault();
-    var text = chatForm.children('input[name=text]').val();
-    chatForm.children('input[name=text]').val(''),
-    $.post('/chat/new', {
-      text: text,
-      idea: chatForm.children('input[name=idea]').val(),
-      user: chatForm.children('input[name=user]').val(),
-    }, function(data) {
-      $('.posts').append('<div class="post">' + data.text + ' - ' + data.time + '</div>');
-    }, 'json');
-  });
+  console.log("Application.js loaded");
 });

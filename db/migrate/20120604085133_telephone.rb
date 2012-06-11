e269ba3c97c0b862dbfb6eac2b0362e47a023ea3
Nat Welch <nat@natwelch.com>
2012-06-10 21:36:37 -0700

    fix telephone def for active record

--- db/migrate/20120604085133_telephone.rb
+++ db/migrate/20120604085133_telephone.rb
@@ -1,7 +1,5 @@
 class Telephone < ActiveRecord::Migration
-  def up
-    alter_table :users do |t|
-      t.string :telephone
-    end
+  def change
+    add_column :users, :telephone, :string
   end
 end

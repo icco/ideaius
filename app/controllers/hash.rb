e9288bee172dfb1c0b860a4db3bd6364822de96f
Nat Welch <nat@natwelch.com>
2012-07-09 22:59:55 -0700

    hash view

--- app/controllers/hash.rb
+++ app/controllers/hash.rb
@@ -1,10 +1,14 @@
 Stackius.controllers :hash do
+  layout :main
+
   get :index do
     404
   end
 
   get :index, :with => :string do
-    @messages = Message.where("text LIKE '%##{params[:string]}%'")
+    @user = logged_in!
+    @hash = "##{params[:string]}"
+    @messages = Message.where("text LIKE '%#{@hash}%'")
 
     return render "hash/index", :locals => { }
   end
